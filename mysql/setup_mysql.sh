5baa5b2c1ba86ae8bce8b1214b6e0d55763e8b04
ehakam <ehakam@student.1337.ma>
2021-02-05 19:03:22 +0100

    Added first YML file (Deployment) :)

--- mysql/setup_mysql.sh
+++ mysql/setup_mysql.sh
@@ -6,7 +6,3 @@ apk		--no-cache add mariadb mariadb-common mariadb-client
 
 /etc/init.d/mariadb setup
 rc-status
-# echo "port = 3306" >> /etc/my.cnf;
-# echo "bind-address = 0.0.0.0" >> /etc/my.cnf;
-# echo "socket=/run/mysqld/mysqld.sock" >> /etc/my.cnf;
-# echo "skip-networking=false" >> /etc/my.cnf;
